080f1f92865276cdb0c96d214a63a9d4ec9ac110
Toth Adam <adamtoth988@gmail.com>
2023-05-22 16:28:54 +0200

    Initial comit

---
+++ src/main/java/hu/nye/prog/phonecatalog/phonecatalog/PhonecatalogApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.prog.phonecatalog.phonecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonecatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonecatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/prog/phonecatalog/phonecatalog/PhonecatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.prog.phonecatalog.phonecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonecatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
